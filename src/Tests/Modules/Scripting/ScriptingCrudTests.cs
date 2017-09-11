8e69c73a59a8099df4c6df1273ae5aa9e2ae717f
Martijn Laarman <Mpdreamz@gmail.com>
2017-09-11 15:56:49 +0200

    Included xunit.runner.visualstudio
    Rider 2017.2 also needs it now :(
    
    The seeder no longer indexes multiple types into a single index,
    mapping.single_type no longer works in index templates (it shouldn't
    either) so we can no longer postpone dealing with single mapping types :)

--- src/Tests/Modules/Scripting/ScriptingCrudTests.cs
+++ src/Tests/Modules/Scripting/ScriptingCrudTests.cs
@@ -21,8 +21,6 @@ namespace Tests.Modules.Scripting
 			requestAsync: (s, c, r) => c.PutScriptAsync(r)
 		);
 
-		private string _lang = "painless";
-
 		protected PutScriptRequest CreateInitializer(string id) => new PutScriptRequest(id) { Script = "1+1" };
 		protected IPutScriptRequest CreateFluent(string id, PutScriptDescriptor d) => d.Script("1+1");
 
